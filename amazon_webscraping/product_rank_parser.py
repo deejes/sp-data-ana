b74a6e91d0648c94e119ea7912a4e815e0222dc9
deejes <j.devansh29@gmail.com>
2018-03-26 18:55:47 -0700

    remove tests

--- amazon_webscraping/product_rank_parser.py
+++ amazon_webscraping/product_rank_parser.py
@@ -1,5 +1,3 @@
-# a = ['\n', 'Amazon Best Sellers Rank:', ' \n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n', '\n.zg_hrsr { margin: 0; padding: 0; list-style-type:none; }\n.zg_hrsr_item { margin: 0 0 0 10px; }\n.zg_hrsr_rank { display: inline-block; width: 80px; text-align: right; }\n', '\n\n', '\n    ', '\n    ', '#47', '\n    ', u'in\xa0', 'Health & Household', ' > ', 'Sexual Wellness', ' > ', 'Adult Toys & Games', ' > ', 'Sex Toys', ' > ', 'Penis Rings', '\n    ', '\n', '\n\n']
-
 def parse_product_rank(li_element_list):
     ### Takes a list of elements from best seller info, returns relevant comma separated values
     li_element_list = [x.strip() for x in li_element_list]
@@ -7,10 +5,4 @@ def parse_product_rank(li_element_list):
     for element in li_element_list[:]:
         if len(element) > 1 and "{" not in element and ":" not in element:
             result.append(element)
-    # import pdb; pdb.set_trace()
     return (",".join(result))
-
-
-
-
-# parse_product_rank(a)
