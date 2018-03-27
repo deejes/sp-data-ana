6569739928bef3a4735d758df5478bedc5b16555
deejes <j.devansh29@gmail.com>
2018-03-26 18:24:43 -0700

    add initalise and write row to csv functions

--- amazon_webscraping/main.py
+++ amazon_webscraping/main.py
@@ -1,20 +1,26 @@
-from lxml import html
+import random
 import csv,os,json
 import requests
 from time import sleep
+from lxml import html
 from parse_amazon_webpage import parse
 from asin_list import AsinList
-import random
+from write_to_file import initalise_write_headers, write_row
+
+
 
 asin_list = []
 for _ in range (10):
     asin_list.append(AsinList[random.randint(0,len(AsinList))])
 
+#TODO - add error logs
+initalise_write_headers()
+
 for i in asin_list:
     url = "http://www.amazon.com/dp/"+i
     print ("Processing: "+url)
     single_url_data = (parse(url))
+    write_row(single_url_data)
     import pdb; pdb.set_trace()
-    write_to_csv(single_url_data)
-    # extracted_data.append(parse(url))
     sleep(1)
+
