3e77b14d716d05aa9f6204bea141537584254aeb
deejes <j.devansh29@gmail.com>
2018-04-28 23:19:12 -0400

    summary commit of unsaved changes

--- optmize_webscraping/amazon_webscraping1/main.py
+++ optmize_webscraping/amazon_webscraping1/main.py
@@ -14,7 +14,8 @@ def echo(input_list, errors_output_file, results_output_file):
         myfile.write("hello results file!")
     
 
-for x in names_list:
+# for x in names_list:
+
 a = [1,2,3]
 b = "errors.txt"
 c = "results.txt"
